403faaf6d62f8be5e073c4cbbcff32ff422e083b
hhenriques1999 <hhenriques1999@gmail.com>
2023-10-01 18:00:49 -0300

    Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,23 @@
+import React from 'react';
+import './Login.css';
+
 function Login() {
 	return (
-		<div>
-			Login Screen
+		<div className="login-container">
+			<h2>Login</h2>
+			<form className="login-form">
+				<div className="form-group">
+					<label htmlFor="usuario">Usuário</label>
+					<input type="text" id="usuario" name="usuario" required />
+				</div>
+				<div className="form-group">
+					<label htmlFor="senha">Senha</label>
+					<input type="senha" id="senha" name="senha" required />
+				</div>
+				<button type="submit">Login</button>
+			</form>
 		</div>
 	);
-}
+};
 
 export default Login;
